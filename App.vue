a3fe733b9afa17ece762160517044c18212d1747
genests <clad1995@gmail.com>
2022-05-02 16:29:39 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,153 +1,74 @@
-<template>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div>
-  <div v-bind:style="image" class="imageaage">
-  <div v-bind:class="menupage">
-      <title>{{ app }}</title>
-      <ul v-bind:style="listli">
-<li v-bind:style="listlis" v-bind:type="circle" v-for="key in menusks" v-bind:title="key"><a v-bind:href="key" v-bind:style="styleObject">{{ key}}</a></li>
-
-
-</ul>
-</div>
-</div>
-    <div v-bind:style="menupage2">
-        <h1>TEST !!!! {{ prop.split('2').reverse().join('3') }}</h1>// 2 на 1
-        <h2>TEST !!!! {{ prop.split('1').reverse().join('2') }}</h2>// 1 на 2
-        <h2>TEST !!!! {{ prop.split('').reverse().join('') }}</h2>
-        <div id="example-1">
-  <button v-on:click="counter += 1">+1</button>
-  <button v-on:click="counter -= 1">-1</button>
-  <p>Кнопка выше была нажата {{ counter }} раз</p>
-
-        <h2>{{ message }}</h2>
-        <h3>{{ test1 }}</h3>
- 
-        <button v-bind:none="isButtonDisabled">Кнопка</button>
-        <input v-model="messager">
-  
-  
+        
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
     
-    <span v-if="seen">Сейчас меня видно3</span>
-    <span v-once>Это сообщение никогда не изменится: </span>
-    <a v-bind:href="url"> Вебсайт </a>
-    <h1>TEST !!!! {{ func1() }}</h1>// 2 на 1
-    <h2>TEST !!!! {{ func2() }}</h2>// 1 на 2
-    <h2>TEST !!!! {{ func3() }}</h2>
-    <h3> {{ tex1() }} </h3>
+    <body class="subpixel-antialiased">
+        <div id="lapp">
+            <app></app>
+        </div>
+    <body class="antialiased">
+        <div id="app">
+            <app></app>
+        </div>
+        <script src="{{ mix('js/app.js')}}"></script>
+    </body>
+</html>
+<?php
+ $user='postgres';
+$pass='qwerty';
+$port='5432';
+$servername='localhost';
+
+
+try {
+    $conn = new PDO("pgsql:host=$servername;port=$port;dbname=postgres", $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
     
-    <h3>{{ now }}</h3>
-
-    <li v-for="todo in ACCE">
-      {{ todo.text }}
-    </li>
- 
-    <button v-on:click="reverseMessage">Перевернуть сообщение</button>
-    </div>
-
-</div>
-</div>
-</template>
-
-<script>
-export default {
-    data() {
-        return {
-            'prop': '1112',
-            'image': 'http://127.0.0.1/img_nature.jpg',
-            'message': 'Вы загрузили эту страницу2: ' + new Date().toLocaleString(),
-            'test1': 'Азора',
-            'app': 'Разработка',
-            textDecoration: 'center',
-             counter: 0,
-            messager: 'Привет, Vue!',
-            menu1: 'Start',
-            menu2: 'Register',
-            menu3: 'Static',
-            url1: '/start',
-            url2: '/reg',
-            url3: '/static',
-            seen: true,
-            circle: "circle",
-            url: "http://www.yandex.ru",
-            styleObject: {
-    color: 'red',
-    textdecoration: 'blue',
-    fontSize: '18px',
-    'text-decoration': 'none'
-  },
-  listli: {
-    margin: '0',
-    'list-style': '0',
-    'padding-left': '50%'
-  },
-    listlis: {
-    float: 'left',
-    'margin-right': '5px'
-  },
-  menupage : {
-  float: 'center',
-  color: '#f2f2f2',
-  'text-align': 'center',
-  padding: '14px 16px',
-  'text-decoration': 'none',
-  'font-size': '17px'
-  },
-  menupage2 : {
-  float: 'left',
-  color: 'red',
-  'text-align': 'left',
-  padding: '50px 50px',
-  'text-decoration': 'none',
-  'font-size': '15px'
-  },
-            ACCE: [
-      { text: 'Изучить JavaScript' },
-      { text: 'Изучить Vue' },
-      { text: 'Создать что-нибудь классное' },
-            ],
-          menusks: {
-        key1: 'Start',
-        key2: 'Register',
-        key3: 'Static',
-        },
-                
-            groceryList: [
-      { id: 0, text: 'Овощи' },
-      { id: 1, text: 'Сыр' },
-      { id: 2, text: 'Что там ещё люди едят?' }
-    ]
-        };
-    },
-    methods: {
-    reverseMessage: function () {
-      this.test1 = this.test1.split('').reverse().join('')
-    },
-    func1: function () {
-    return this.prop.split('2').reverse().join('3')
-  },
-  func2: function () {
-    return this.prop.split('1').reverse().join('2')
-  },
-  func3: function () {
-    return this.prop.split('').reverse().join('')
-  },
-  tex1: function(){
-    return this.test1 + ' ' + this.message.split('2022').reverse().join('')
-  }},
-  computed: {
-  now() {
-    let _date = new Date()
-    return _date.toLocaleString()
-  },
-  isButtonDisabled() {
-        // you can  check your form is filled or not here.
-  
-  },
-  imageaage: function() {
-            return {'background-image': `url(${this.image})`}
-        }
+    
+$sqldd = "SELECT * from users where active='1';";
+$sqldd2 = "SELECT id,username,active from users;";
+$stmt = $conn->prepare($sqldd);
+$stmt->execute();
+$stmt2 = $conn->prepare($sqldd2);
+$stmt2->execute();
+$stmt3 = $conn->prepare($sqldd2);
+$stmt3->execute();
+$active = '1';
+$sql = 'SELECT * FROM users  ORDER BY username' ; // where active = 1
+foreach ($conn->query($sql) as $row) {
+    print 'ID:' . $row['id'] . "\t";
+    print 'USERNAME:' . $row['username'] . "\t";
+    print 'ACTIVE:' . $row['active'] . "\n";
 }
-};
+$result = $stmt->fetchAll();
+$result2 = $stmt2->fetchColumn(1);
+print_r($result);
+$stmt->closeCursor();
+print_r($result2);
+$stmt2->closeCursor();
+$colcount = $stmt3->columnCount();
+print("\nКоличество в базе:");
+print_r($colcount);
+$stmt3->closeCursor();
+print("\n");
 
-</script>
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+
+
+
+ ?>
