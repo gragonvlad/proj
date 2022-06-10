8517c7cf935f3a036b67f7fe9b4f287a6ede5b0d
genests <clad1995@gmail.com>
2022-06-10 19:10:38 +0300

    Update test.blade.php

--- test.blade.php
+++ test.blade.php
@@ -55,10 +55,15 @@ $stmt3 = $conn->prepare($sqldd2);
 $stmt3->execute();
 $active = '1';
 $sql = 'SELECT * FROM users  ORDER BY username' ; // where active = 1
+print '/n';
 foreach ($conn->query($sql) as $row) {
     print 'ID:' . $row['id'] . "\t";
     print 'USERNAME:' . $row['username'] . "\t";
     print 'ACTIVE:' . $row['active'] . "\n";
+    $format = '<nav class="top-menu"><ul class="menu-main"><li class="left-item"><a href="%s">%s</a></li></ul><a class="navbar-logo" href=""></a></nav>';
+    $active = $row['id'];
+    $user = $row['username'];
+echo sprintf($format, $active, $user, "/n");
 }
 $result = $stmt->fetchAll();
 $result2 = $stmt2->fetchColumn(1);
@@ -98,15 +103,47 @@ $stht4->execute([':active' => $active, ':username' => $username]);
 echo "<pre>";
 $rawDb = $stht4->fetchAll(PDO::FETCH_ASSOC);
 
-$id = $rawDb[0]['id'];
-$active = $rawDb[0]['active'];
+
 
 $format = '<form method="POST" action=%s><input type="text" name="name"/><br/></form>';
 echo sprintf($format, $active, "/n");
 
 echo "</pre>";
 $stht4->closeCursor();
-
+$formatll = '<header class="page-header">
+<nav class="navigation">
+  <a class="logo-link" href="#" aria-current="page">
+    <img class="logo-img" src="images/logo.svg" width="139" height="70" alt="Логотип Седоны">
+  </a>
+  <ul class="navigation-list">
+    <li class="navigation-item">
+      <a class="navigation-link navigation-link-current" href="#">%s</a>
+    </li>
+    <li class="navigation-item">
+      <a class="navigation-link" href="#">О Седоне</a>
+    </li>
+    <li class="navigation-item">
+      <a class="navigation-link" href="catalog.html">Гостиницы</a>
+    </li>
+  </ul>
+  <ul class="navigation-list navigation-user">
+    <li class="navigation-item">
+      <a class="navigation-link" href="#">
+        <span class="visually-hidden">Поиск</span>
+      </a>
+    </li>
+    <li class="navigation-item">
+      <a class="navigation-link" href="#">
+        <span class="visually-hidden">Избранное</span>
+      </a>
+    </li>
+    <li class="navigation-item">
+      <button class="btn btn-brown navigation-btn" type="button">Хочу сюда!</button>
+    </li>
+  </ul>
+</nav>
+</header>';
+echo sprintf($formatll, $active, "/n");
 
 
 
